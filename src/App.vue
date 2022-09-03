70058729d63e5ffeaa35614480416a68db720e90
Johan Sebastian Sanchez Navas <johan_s.sanchez@uao.edu.co>
2022-09-03 10:36:07 -0500

    Pagina

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
-    <AmbienteWeb/>  
-</template>
-
-<script>
+    <!-- <AmbienteWeb/>
+    <Restaurantes/>
+    <Mercados/>
+    <Farmacias/>
+    <Tiendas/>
+    <Bares/>
+    <Recreacion/>
+    <Hospedajes/>   
+    <Restaurante/>
+    <RegistrarRestaurante/> -->
+    <RegistrarEstablecimiento/>
+ </template>
+ 
+ <script>
 import AmbienteWeb from './components/AmbienteWeb.vue';
-    export default{
-        name:'App',
-        components:{
-            AmbienteWeb
-        }
-    }
-</script>
-
-<style>
-</style>
+ import Restaurantes from './components/Restaurantes.vue';
+import Mercados from './components/Mercados.vue';
+import Farmacias from './components/Farmacias.vue';
+import Tiendas from './components/Tiendas.vue';
+import Bares from './components/Bares.vue';
+import Recreacion from './components/Recreacion.vue';
+import Hospedajes from './components/Hospedajes.vue'; 
+import Restaurante from './components/Restaurante.vue';
+import RegistrarEstablecimiento from "./components/RegistrarEstablecimiento.vue";
+import RegistrarRestaurante from "./components/RegistrarRestaurante.vue";
+     export default{
+         name:'App',
+         components:{
+    AmbienteWeb,
+    Restaurantes,
+    Mercados,
+    Farmacias,
+    Tiendas,
+    Bares,
+    Recreacion,
+    Hospedajes,
+    Restaurante,
+    RegistrarEstablecimiento,
+    RegistrarRestaurante
+}
+     }
+ </script>
+ <style>
+     
+ </style>
+ 
